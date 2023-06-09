0f9c387d96aa847c542a673a0cc4979b5bd61c9e
SaranE1 <110584461+SaranE1@users.noreply.github.com>
2023-06-09 23:10:59 +0530

    ex5.py

--- ex5.py
+++ ex5.py
@@ -1,9 +1,21 @@
+#import json file
+
 import json
+
+#open the json file 
+#store it in dictionary
+#append the content need to be added
+#print it to check
+
 
 with open('ex5.json') as json_file:
     ex5 = json.load(json_file)
     ex5[2]['batters']['batter'].append({'id': '1003', 'type': 'Coffee'})
     print(ex5)
+
+
+#open the file in Write mode
+#dumb the updated value to the existing json file
 
 
 with open('ex5.json','w') as json_file:
